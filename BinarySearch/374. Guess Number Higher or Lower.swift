dfa75741db5deda62e4ee4b8fa90aa5cf699873a
Madhur Jain <jmadhur.kothadar42@gmail.com>
2025-03-24 12:24:00 +0530

    Create 374. Guess Number Higher or Lower.swift

---
+++ BinarySearch/374. Guess Number Higher or Lower.swift
@@ -0,0 +1,23 @@
+
+// https://leetcode.com/problems/guess-number-higher-or-lower/
+
+class Solution : GuessGame {
+    func guessNumber(_ n: Int) -> Int {
+        var start = 0 
+        var end = n
+        var mid = start + (end - start)/2
+
+        while start <= end {
+            var guess = guess(mid)
+            if guess == 0 {
+                return mid
+            } else if guess == 1 {
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+            mid = start + (end - start)/2
+        }
+        return 0
+    }
+}
