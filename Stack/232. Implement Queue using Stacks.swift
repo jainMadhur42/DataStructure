428c30d83718e6aea266969fd2619af75dfba27d
Madhur Jain <jmadhur.kothadar42@gmail.com>
2025-03-30 18:59:06 +0530

    Create 232. Implement Queue using Stacks.swift

---
+++ Stack/232. Implement Queue using Stacks.swift
@@ -0,0 +1,60 @@
+
+// https://leetcode.com/problems/implement-queue-using-stacks/
+
+
+
+// Use recursion as second stack.
+class MyQueue {
+
+    var stack: [Int] = []
+  
+    init() {
+        
+    }
+    
+    func push(_ x: Int) {
+        stack.append(x)    
+    }
+    
+    func pop() -> Int {
+       if stack.isEmpty {
+            return nil
+        }
+
+        var last = stack.popLast()
+        if stack.isEmpty {
+            return last
+        }
+        let front = solve()
+        stack.append(last!)
+        return front
+    }
+    
+    func peek() -> Int {
+        if stack.isEmpty {
+            return -1
+        }
+
+        var last = stack.popLast()
+        if stack.isEmpty {
+            stack.append(last!)
+            return last!
+        }
+        let front = peek()
+        stack.append(last!)
+        return front
+    }
+    
+    func empty() -> Bool {
+        return stack.isEmpty 
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * let obj = MyQueue()
+ * obj.push(x)
+ * let ret_2: Int = obj.pop()
+ * let ret_3: Int = obj.peek()
+ * let ret_4: Bool = obj.empty()
+ */
