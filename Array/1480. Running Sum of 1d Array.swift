e0315198b49cf071f7525256cbac5c65135a7b68
Madhur Jain <jmadhur.kothadar42@gmail.com>
2025-03-31 17:24:06 +0530

    Update 1480. Running Sum of 1d Array.swift

--- Array/1480. Running Sum of 1d Array.swift
+++ Array/1480. Running Sum of 1d Array.swift
@@ -1,7 +1,9 @@
 
 
 // https://leetcode.com/problems/running-sum-of-1d-array/
-// This is called prefix sum problem
+// This is called the prefix sum problem
+// https://medium.com/@maityamit/prefix-sum-summary-with-practice-questions-sheet-1d-2d-on-leetcode-83c8deb4f713
+
 class Solution {
     func runningSum(_ nums: [Int]) -> [Int] {
         var result: [Int] = []
